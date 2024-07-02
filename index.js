69e2c6449b16b09bc4aa6df1bc28eadc42d5a5d4
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-07-02 09:47:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function showSidebar(event) {
+    event.preventDefault();
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    event.preventDefault();
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+function updateYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('current-year').innerText = currentYear;
+}
+
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('current-time').innerText = currentTime;
+}
+
+updateYear();
+updateTime();
+
+setInterval(updateTime, 1000);
